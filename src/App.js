c10dcc3c66acb6a67c2737d75d71899fa9533870
Kyle Greenidge <kylegreen149@gmail.com>
2025-03-03 17:43:10 -0500

    Creates a simple onChange event handler that keeps track of the user input

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([])
+  const [input, setInput] = useState("")
+
   return (
     <div className="App">
       <h1>To-Do List</h1>
       <h2>Create Tasks and Remove Them from List Once Completed</h2>
-      <input placeholder='Enter a Task...'></input>
+      <input 
+      type="text"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      placeholder='Enter a Task...'/>
       <button>Add Task</button>
     </div>
   );
